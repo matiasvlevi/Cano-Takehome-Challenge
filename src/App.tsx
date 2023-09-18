7fdaf5f5d29f21997c2cc8d8b5178493d9064922
vlev <matiasvlevi@gmail.com>
2023-09-18 12:48:37 -0400

    nope

--- src/App.tsx
+++ src/App.tsx
@@ -25,22 +25,17 @@ function App() {
    */
   const toggleSortMode = () => {
     // Toggle the sort mode
-    setSortMode((prev) => (prev === "asc" ? "desc" : "asc"));
+    setSortMode((prev: SortMode) => (prev === "asc" ? "desc" : "asc"));
   };
 
-  /**
-   * Sort by name once the search value was updated
-   *
-   * (Sorting the products in the onChange event would be 1 character behind,
-   *  so we need to use useEffect and sort after search was updated)
-   */
-  useEffect(() => {
+  const handleSearch = (e: any) => {
+    setSearch(e.target.value)
     // Sort products by name
     setProducts(sortProductsByStringMatch(products, "name", search));
 
     // Cancel score sort
     setSortMode("none");
-  }, [search]);
+  }
 
   /**
    * Sort by score once the sort mode was updated
@@ -58,7 +53,7 @@ function App() {
   return (
     <main style={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
       <div className="TopTray">
-        <SearchBar value={search} onChange={(e) => setSearch(e.target.value)} />
+        <SearchBar value={search} onChange={handleSearch} />
         <button onClick={toggleSortMode}>Sort {sortIcons[sortMode]}</button>
       </div>
       <div className="scrollBox">
