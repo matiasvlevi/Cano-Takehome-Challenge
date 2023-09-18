b7a606091fb09a73402da8f5abbf6bf9ee77ea54
vlev <matiasvlevi@gmail.com>
2023-09-18 11:58:50 -0400

    Only the static build

--- src/sortIcons.ts
+++
@@ -1,5 +0,0 @@
-export const sortIcons: Record<string, string> = {
-  asc: "△",
-  desc: "▽",
-  none: "―",
-};
--- src/types.ts
+++
@@ -1,6 +0,0 @@
-export interface Product {
-  name: string;
-  environmentalImpactScore: number;
-}
-
-export type SortMode = "asc" | "desc" | "none";
